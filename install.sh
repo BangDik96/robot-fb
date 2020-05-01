868e2efb3647d83a95e228c5edbdd983a17b3cf5
ᏂᏋᏦᏋᏝᎮᏒᎧ <62897038+hekelpro@users.noreply.github.com>
2020-05-01 14:42:37 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg update && pkg upgrade
+pkg install python
+pkg install git
+pip install --upgrade pip
+pip install requests
+pip install mechanize
+pip install cookiejar
+pip install bs4
+pip install clint
+pip install prompt_toolkit
+pkg install unzip
+unzip robot.zip
